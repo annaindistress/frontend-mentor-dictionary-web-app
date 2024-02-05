b2e7954956fe39eed30f3b379838f4d3189caf54
Anna Artemova <annartmva@gmail.com>
2024-02-05 11:27:29 +0400

    Fix style errors

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -2,13 +2,12 @@ import { useDictionary } from "../context/DictionaryProvider";
 import WordHeader from "./WordHeader";
 import WordMeaning from "./WordMeaning";
 import WordFooter from "./WordFooter";
-import styles from "./Word.module.css";
 
 export default function Word() {
   const { word } = useDictionary();
 
   return (
-    <section className={styles.section}>
+    <section>
       <WordHeader />
       {word.meanings.map((meaning) => (
         <WordMeaning key={meaning.partOfSpeech} meaning={meaning} />
